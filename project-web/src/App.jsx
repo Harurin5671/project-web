e6dc6f5057d1aa12b793f8d56d73b3ee0870dbba
Harurin <frankcito639@gmail.com>
2024-02-10 10:51:53 -0500

    iniciando el projecto con React-vite

---
+++ project-web/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p style={{fontSize: '80px'}}>
+        Hello World!
+      </p>
+    </>
+  )
+}
+
+export default App
